811832f7a822e3558c2a9fb4ccd5b62c1612a432
JulianBu <j.bueche93@gmx.de>
2021-06-04 16:20:55 +0200

    Started with Actionbar for some activities

--- app/src/main/java/com/example/quizapp/MainViewModelFactory.kt
+++ app/src/main/java/com/example/quizapp/MainViewModelFactory.kt
@@ -3,10 +3,12 @@ package com.example.quizapp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.quizapp.repository.Repository
+import kotlin.Suppress
 
 class MainViewModelFactory(private val repository: Repository): ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        @Suppress("UNCHECKED_CAST")
         return MainViewModel(repository) as T
     }
 }
--- app/src/main/java/com/example/quizapp/UserScoreActivity.kt
+++ app/src/main/java/com/example/quizapp/UserScoreActivity.kt
@@ -1,25 +1,28 @@
 package com.example.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.appcompat.app.AppCompatActivity
 import com.example.quizapp.util.GlobalValues
+
 
 class UserScoreActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_score)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#e5e5e2")))
 
         val pic1 = R.drawable.cliofront
         val pic2 = R.drawable.fahrwerk
         val pic3 = R.drawable.mx5
         val pic4 = R.drawable.m240i
-        val pictureList: ArrayList<Int> = ArrayList<Int>(4)
+        val pictureList = ArrayList<Int>(4)
         pictureList.add(pic1)
         pictureList.add(pic2)
         pictureList.add(pic3)
@@ -34,6 +37,15 @@ class UserScoreActivity : AppCompatActivity() {
         user.setText(userInfo)
         score.setText(GlobalValues.currentScore.toString())
 
+    }
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
